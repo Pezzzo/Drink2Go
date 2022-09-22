7a564c4cef3ba4efff41bf9618ec1b2779a823d2
Pezzzo <watchingthewaves.mix@gmail.com>
2022-09-22 15:32:51 +0300

    исправление ошибок вёрстки, более правильное расположение файлов sass

--- source/js/index.js
+++ source/js/index.js
@@ -20,26 +20,6 @@ navToggle.addEventListener('click', () => {
   navToggle.classList.toggle('main-nav__burger--closed');
 });
 
-
-// список сортировки
-selected.addEventListener('click', () => {
-  optionsContainer.classList.toggle('active');
-});
-
-optionsList.forEach(el => {
-  el.addEventListener('click', () => {
-    selected.textContent = el.textContent;
-    optionsContainer.classList.remove('active');
-  });
-});
-
-document.body.addEventListener('click', (evt) => {
-  const close = evt.target.closest('.sorting-form__selected');
-  if (!close) {
-    optionsContainer.classList.remove('active');
-  }
-});
-
 // Слайдер
 try {
   new Swiper('.swiper', {
@@ -65,36 +45,41 @@ catch (err) {
 }
 
 // Карта
-const map = L.map('map')
-  .setView({
-    lat: 59.96831,
-    lng: 30.31748,
-  }, 16);
+try {
+  const map = L.map('map')
+    .setView({
+      lat: 59.96831,
+      lng: 30.31748,
+    }, 16);
 
-L.tileLayer(
-  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-  {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>',
-  },
-).addTo(map);
+  L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>',
+    },
+  ).addTo(map);
 
-const mainPinIcon = L.icon({
-  iconUrl: 'img/map-marker.svg',
-  iconSize: [38, 50],
-  iconAnchor: [26, 52],
-});
+  const mainPinIcon = L.icon({
+    iconUrl: 'img/map-marker.svg',
+    iconSize: [38, 50],
+    iconAnchor: [26, 52],
+  });
 
-const mainPinMarker = L.marker(
-  {
-    lat: 59.96831,
-    lng: 30.31748,
-  },
-  {
-    draggable: true,
-    icon: mainPinIcon,
-  },
-);
+  const mainPinMarker = L.marker(
+    {
+      lat: 59.96831,
+      lng: 30.31748,
+    },
+    {
+      draggable: true,
+      icon: mainPinIcon,
+    },
+  );
 
-mainPinMarker
-  .addTo(map)
-  .bindPopup("Drink2Go - Интернет магазин кофейных напитков");
+  mainPinMarker
+    .addTo(map)
+    .bindPopup("Drink2Go - Интернет магазин кофейных напитков");
+}
+catch (err) {
+  console.error(err);
+}
